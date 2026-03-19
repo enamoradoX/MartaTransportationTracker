9143c689085b2fb3e8d6e73b8a609d7f302af400
GZ1Z2N <christian.enamorado@gm.com>
2026-03-19 13:39:12 -0400

    Added basic front end for tracking trains. Connected to the back end for real time data.

--- marta-backend/src/main/java/com/marta/tracker/martatransportationtracker/controller/MartaTrainController.java
+++ marta-backend/src/main/java/com/marta/tracker/martatransportationtracker/controller/MartaTrainController.java
@@ -1,9 +1,28 @@
 package com.marta.tracker.martatransportationtracker.controller;
 
+import com.marta.tracker.martatransportationtracker.model.MartaTrain;
+import com.marta.tracker.martatransportationtracker.service.MartaService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api")
 public class MartaTrainController {
 
+    private MartaService service;
+
+    public MartaTrainController(MartaService service){
+        this.service = service;
+    }
+
+    @GetMapping(value = "/trains")
+    public List<MartaTrain> getTrains(){
+        return service.getLiveTrains();
+    }
 
 }
--- marta-frontend/src/app/app.module.ts
+++ marta-frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MartaMapComponent } from './marta-map/marta-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MartaMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
